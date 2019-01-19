ade3583051db3d922a01e2e25e96b1707592045b
yoshi <yoshiaki._1229@ezweb.ne.jp>
2019-01-19 16:44:59 +0900

    initial

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navigation = ({onRouteChange, isSignedIn}) => {
+  if (isSignedIn) {
+    return (
+      <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={() =>onRouteChange('signin')} className="f3 link dim black underline pa3 pointer">Sign out</p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={() =>onRouteChange('signin')} className="f3 link dim black underline pa3 pointer">Sign in</p>
+        <p onClick={() =>onRouteChange('resister')} className="f3 link dim black underline pa3 pointer">Resister</p>
+      </nav>
+    );
+  }
+}
+
+
+
+export default Navigation;
